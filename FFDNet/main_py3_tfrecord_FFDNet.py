022b683b2f469c7687095b299c329506d364e21b
Dani Fuya <danifuya@gmail.com>
2019-06-10 21:05:43 +0200

    FFDNet lr arreglada

--- FFDNet/main_py3_tfrecord_FFDNet.py
+++ FFDNet/main_py3_tfrecord_FFDNet.py
@@ -20,7 +20,7 @@ parser.add_argument('--checkpoint_dir', dest='ckpt_dir', default='./checkpoint',
 parser.add_argument('--sample_dir', dest='sample_dir', default='./sample', help='sample are saved here')
 parser.add_argument('--test_dir', dest='test_dir', default='./test', help='test sample are saved here')
 parser.add_argument('--eval_set', dest='eval_set', default='test', help='dataset for eval in training')
-parser.add_argument('--test_set', dest='test_set', default='deblocking', help='dataset for testing')
+parser.add_argument('--test_set', dest='test_set', default='test', help='dataset for testing')
 args = parser.parse_args()
 
 #weigth decay momentum optimizer
@@ -36,8 +36,8 @@ def denoiser_train(denoiser, lr, eval_every_step, patch_size):
     denoiser.train(img_labelBatch, img_bayerBatch, eval_data_gt, eval_data_bl, batch_size=args.batch_size, ckpt_dir=args.ckpt_dir, lr=lr, sample_dir=args.sample_dir, eval_every_step=eval_every_step)
 
 def denoiser_test(denoiser):
-    test_files_gt = glob('../test/{}/groundtruth/*'.format(args.test_set))
-    test_files_bl = glob('../test/{}/compressed/*'.format(args.test_set))
+    test_files_gt = glob('../images/{}/groundtruth/*'.format(args.test_set))
+    test_files_bl =glob(('../images/{}/compressed_Q' + args.quantization_step +'/*').format(args.test_set))
     denoiser.test(test_files_gt, test_files_bl, ckpt_dir=args.ckpt_dir, save_dir=args.test_dir)
 
 def ensemble_test(denoiser):
@@ -67,16 +67,13 @@ def main(_):
                 iter_epoch = numPatches//args.batch_size
                 iter_all = args.epoch*iter_epoch
                 lr = args.lr * np.ones(iter_all) 
-                lr[iter_epoch*10:] = lr[0] / 2.0
-                lr[iter_epoch*20:] = lr[0] / 10.0
-                #lr[iter_epoch*30:] = lr[0] / 20.0
-                # lr[iter_epoch*40:] = lr[0] / 100.0
-                # lr[iter_epoch*25:] = lr[0] / 200.0
-                # lr[iter_epoch*30:] = lr[0] / 1000.0
-                # lr[iter_epoch*35:] = lr[0] / 2000.0
-                # lr[iter_epoch*40:] = lr[0] / 10000.0
-                # lr[iter_epoch*45:] = lr[0] / 10000.0
-
+                lr[iter_epoch*7:] = lr[0] / 2.0
+                lr[iter_epoch*14:] = lr[0] / 10.0
+                lr[iter_epoch*21:] = lr[0] / 20.0
+                lr[iter_epoch*28:] = lr[0] / 100.0
+                lr[iter_epoch*35:] = lr[0] / 200.0
+                lr[iter_epoch*42:] = lr[0] / 1000.0
+                               
                 denoiser_train(model, lr=lr, eval_every_step=iter_epoch, patch_size=args.patch_size)
             elif args.phase == 'test':
                 denoiser_test(model)
